ca6171fb9ad8f946843d4acec8eaaec1c2b084a1
arsyee <2476948+arsyee@users.noreply.github.com>
2018-03-31 15:40:05 +0200

    notification shown

--- app/src/main/java/android/example/com/squawker/fcm/SquawkerService.java
+++ app/src/main/java/android/example/com/squawker/fcm/SquawkerService.java
@@ -1,9 +1,16 @@
 package android.example.com.squawker.fcm;
 
+import android.annotation.SuppressLint;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.ContentValues;
+import android.content.Context;
+import android.example.com.squawker.R;
 import android.example.com.squawker.provider.SquawkContract;
 import android.example.com.squawker.provider.SquawkProvider;
-import android.os.AsyncTask;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
@@ -28,7 +35,7 @@ public class SquawkerService extends FirebaseMessagingService {
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        // TODO (3) As part of the new Service - If there is message data, get the data using
+        // COMPLETED (3) As part of the new Service - If there is message data, get the data using
         // the keys and do two things with it :
         Map<String, String> data = remoteMessage.getData();
         if (data != null) {
@@ -36,6 +43,25 @@ public class SquawkerService extends FirebaseMessagingService {
             // 1. Display a notification with the first 30 character of the message
             // If you don't know how to make notifications or interact with a content provider
             // look at the notes in the classroom for help.
+            String channelId = getString(R.string.channel_name);
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                String description = getString(R.string.channel_description);
+                int importance = NotificationManagerCompat.IMPORTANCE_DEFAULT;
+                @SuppressLint("WrongConstant") NotificationChannel channel = new NotificationChannel(channelId, channelId, importance);
+                channel.setDescription(description);
+                NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                notificationManager.createNotificationChannel(channel);
+            }
+
+            NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, channelId)
+                    .setSmallIcon(R.drawable.ic_duck)
+                    .setContentTitle(getString(R.string.app_name))
+                    .setContentText(shortMessage(data.get("message")))
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+            NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
+            notificationManagerCompat.notify(0, notificationBuilder.build());
 
             // 2. Use the content provider to insert a new message into the local database
             // Hint: You shouldn't be doing content provider operations on the main thread.
@@ -54,6 +80,29 @@ public class SquawkerService extends FirebaseMessagingService {
         }
 
         super.onMessageReceived(remoteMessage);
+    }
+
+    private String shortMessage(String message) {
+        final int MAX = 30 - 3;
+        StringBuilder shortMessage = new StringBuilder();
+        for (String word : message.split(" ")) {
+            int remaining = MAX - shortMessage.length();
+            if (remaining - 1 > word.length()) {
+                if (shortMessage.length() != 0) shortMessage.append(" ");
+                shortMessage.append(word);
+            } else if (remaining > MAX / 2) {
+                if (shortMessage.length() != 0) shortMessage.append(" ");
+                if (remaining + 3 > word.length()) {
+                    shortMessage.append(word);
+                } else {
+                    shortMessage.append(word.substring(0, remaining));
+                }
+            }
+        }
+        if (message.length() > shortMessage.length()) {
+            shortMessage.append("...");
+        }
+        return shortMessage.toString();
     }
 
 }
